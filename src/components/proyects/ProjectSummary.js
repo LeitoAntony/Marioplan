a9cc3b98736b0f71177bb3ec08d55ce3a38a9ae1
Leonardo <leitoantony86@gmail.com>
2019-03-29 22:56:13 -0300

    ProjectSummary

---
+++ src/components/proyects/ProjectSummary.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ProjectSummary = () =>{
+    return(
+        <div className="card project-summary my-2">
+                <div className="card-body">
+                    <h5 className="card-title">Tiitulo del proyecto</h5>
+                    <p className="card-text">Posteado por ninja Net</p>
+                    <p className="card-text secondary">3rd de septiembre, 2am</p>
+                    
+                </div>
+            </div>
+    )
+}
+
+export default ProjectSummary;
--- src/components/proyects/ProjectsList.js
+++ src/components/proyects/ProjectsList.js
@@ -1,34 +1,13 @@
 import React from 'react';
+import ProjectSummary from './ProjectSummary';
 
 const ProjectsList = () => {
     return (
         <div className="project-list section">
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Tiitulo del proyecto</h5>
-                    <p className="card-text">Posteado por ninja Net</p>
-                    <p className="card-text secondary">3rd de septiembre, 2am</p>
-                    
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Tiitulo del proyecto</h5>
-                    <p className="card-text">Posteado por ninja Net</p>
-                    <p className="card-text secondary">3rd de septiembre, 2am</p>
-                    
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">Tiitulo del proyecto</h5>
-                    <p className="card-text">Posteado por ninja Net</p>
-                    <p className="card-text secondary">3rd de septiembre, 2am</p>
-                    
-                </div>
-            </div>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div>
     )
 }
