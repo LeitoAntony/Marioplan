aa6883a2d1e9a58b509dcdd76676f9c444e21c59
Leonardo <leitoantony86@gmail.com>
2019-04-02 21:29:32 -0300

    adding data to database

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,18 @@
 export const createProject = (project) => {
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to database
-        dispatch({ type: 'CREATE_PROJECT', project })
-
+        //con esta constante manejo la interaccion con la base
+        const firestore = getFirestore();
+        firestore.collection('projects').add({
+            ...project,
+            authorFirstName: 'Net',
+            authorLastName: 'Ninja',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PROJECT', project });
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PROJECT_ERROR', err });
+        })
     }
-}
+};
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,17 +1,23 @@
 const initState = {
     projects: [
-        {id: '1', title: 'help me find pech', content: 'blah blah blah blah 1'},
-        {id: '2', title: 'collect all the starts', content: 'blah blah blah blah 2'},
-        {id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah blah 3'}
+        { id: '1', title: 'help me find pech', content: 'blah blah blah blah 1' },
+        { id: '2', title: 'collect all the starts', content: 'blah blah blah blah 2' },
+        { id: '3', title: 'egg hunt with yoshi', content: 'blah blah blah blah 3' }
     ]
 }
 
-const projectReducer = (state = initState, action) =>{
-    switch(action.type){
+const projectReducer = (state = initState, action) => {
+    switch (action.type) {
         case 'CREATE_PROJECT':
             console.log("Created project", action.project);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('CREATE_PROJECT_ERROR', action.err);
+            return state;
+        default:
+            return state;
     }
-    return state;
+    
 }
 
 export default projectReducer;
